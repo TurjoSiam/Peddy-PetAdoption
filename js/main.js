e0cefc0045c75f0f73f4c25582f8a9171aa8de9e
TurjoSiam <t.siam24@gmail.com>
2024-10-07 09:25:14 +0600

    modal function added

--- js/main.js
+++ js/main.js
@@ -22,15 +22,77 @@ const loadPets = () => {
     }
 };
 
+// load pet category wise function
 const loadCategoryPets =(categoryName) =>{
     try {
         fetch(`https://openapi.programming-hero.com/api/peddy/category/${categoryName}`)
             .then(res => res.json())
-            .then(data => displayPets(data.data));
+            .then(data => {
+                removeActiveBtn();
+                const activeBtn = document.getElementById(`btn-${categoryName}`);
+                activeBtn.classList.add('active-btn')
+                displayPets(data.data);
+            });
     } 
     catch (error) {
         console.log(error);
     }
+}
+
+// active button class remove
+const removeActiveBtn = () => {
+    const buttons = document.getElementsByClassName('category-btn')
+    for(let button of buttons){
+        button.classList.remove('active-btn')
+    }
+}
+
+// load pet details function
+const loadPetDetails = async (petId) => {
+    const uri = `https://openapi.programming-hero.com/api/peddy/pet/${petId}`
+    const res = await fetch(uri);
+    const data = await res.json();
+    displayDetails(data.petData)
+}
+
+const displayDetails = (details) => {
+    console.log(details);
+    const detailsContainer = document.getElementById('modal-container');
+
+    detailsContainer.innerHTML = `
+    <div class="flex flex-col gap-5">
+        <img src="${details.image}" class="rounded-lg" alt="pet photo">
+        <div class="flex flex-col gap-3">
+            <div>
+                <h2 class="text-2xl font-extrabold">${details.pet_name}</h2>
+            </div>
+            <div class="flex items-center gap-7 border-b pb-3">
+                <div>
+                    ${details.breed == undefined? `<p class="text-gray-500 flex items-center gap-3"><span><i
+                                class="fa-solid fa-table-cells-large"></i></span><span>Breed: Not available</span></p>` : `<p class="text-gray-500 flex items-center gap-3"><span><i class="fa-solid fa-table-cells-large"></i></span><span>Breed: ${details.breed}</span></p>`}
+
+                    ${details.gender == undefined? `<p class="text-gray-500 flex items-center gap-3"><span><i
+                                class="fa-solid fa-transgender"></i></span><span>Gender: Not available</span></p>` : `<p class="text-gray-500 flex items-center gap-3"><span><i class="fa-solid fa-transgender"></i></span><span>Gender: ${details.gender}</span></p>`}
+
+                    ${details.vaccinated_status == undefined? `<p class="text-gray-500 flex items-center gap-3"><span><i class="fa-solid fa-syringe"></i></span><span>Gender: Not available</span></p>` : `<p class="text-gray-500 flex items-center gap-3"><span><i class="fa-solid fa-syringe"></i></span><span>Vaccinated status: ${details.vaccinated_status}</span></p>`}
+                </div>
+                <div>
+                    ${details.date_of_birth == undefined? `<p class="text-gray-500 flex items-center gap-3"><span><i
+                                class="fa-regular fa-calendar-days"></i></span><span>Birth: Not available</span></p>` : `<p class="text-gray-500 flex items-center gap-3"><span><i class="fa-regular fa-calendar-days"></i></span><span>Birth: ${details.date_of_birth}</span></p>`}
+
+                    ${details.price == undefined? `<p class="text-gray-500 flex items-center gap-4"><span><i
+                                class="fa-solid fa-dollar-sign"></i></span><span>Price: Not available</span></p>` : `<p class="text-gray-500 flex items-center gap-4"><span><i class="fa-solid fa-dollar-sign"></i></span><span>Price: ${details.price}</span></p>`}
+                </div>
+            </div>
+            <div class="flex flex-col gap-2">
+                <h3 class="font-bold">Details Information</h3>
+                <p class="text-gray-500">${details.pet_details}</p>
+            </div>
+        </div>
+    </div>
+    `
+
+    document.getElementById('modal').showModal()
 }
 
 
@@ -46,7 +108,7 @@ const displayCategories = (categories) => {
         // create a button
         const div = document.createElement('div');
         div.innerHTML = `
-            <button class="flex items-center gap-3 border px-14 rounded-xl py-3" onclick="loadCategoryPets('${item.category}')">
+            <button id="btn-${item.category}" class="flex items-center gap-3 border px-14 rounded-xl py-3 category-btn" onclick="loadCategoryPets('${item.category}')">
                 <img src="${item.category_icon}" class="w-10" alt="category icons">
                 <h2 class="text-xl font-extrabold">${item.category}</h2>
             </button>
@@ -68,8 +130,7 @@ const displayPets = (pets) => {
                 <img src="./images/error.webp" alt="no content here">
                 <h1 class="text-4xl font-extrabold text-center">No Information Available</h1>
                 <p class="text-gray-500 text-center">Pet adoption is the process of transferring responsibility for a pet that was previously owned by another party sources for adoptable pets.</p>
-            </div>
-        `
+            </div>`
         return
     }
     else{
@@ -102,7 +163,7 @@ const displayPets = (pets) => {
                 <div class="flex justify-between items-center w-full">
                     <div class="rounded-md border py-1 px-3 text-gray-500"><i class="fa-regular fa-thumbs-up"></i></i></div>
                     <div class="rounded-md border py-1 px-3 text-cyan-700 font-bold">Adopt</div>
-                    <div class="rounded-md border py-1 px-3 text-cyan-700 font-bold">Details</div>
+                    <button onclick="loadPetDetails(${item.petId})"><div class="rounded-md border py-1 px-3 text-cyan-700 font-bold">Details</div></button>
                 </div>
         `
         petContainer.append(card);
